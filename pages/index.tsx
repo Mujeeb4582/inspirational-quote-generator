ea285475ac84a0a50d63adcb921123b5a47e3bb1
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-10-11 06:06:58 +0500

    Add styled first component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '@/styles/Home.module.css'
+import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
 
 export default function Home() {
   return (
@@ -11,10 +12,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div>Hello World</div>
-        <h3>Great job running next.js!</h3>
-      </main>
+      {/* Background */}
+      <GradientBackgroundCon></GradientBackgroundCon>
     </>
   )
 }
