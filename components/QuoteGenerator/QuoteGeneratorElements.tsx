4aff7927e3c77def7e6f4bfe4b2e784461adbed9
Mujeeb4582 <mujeeburahman4582@gmail.com>
2023-10-11 11:55:47 +0500

    Add backGround Image and background color

--- components/QuoteGenerator/QuoteGeneratorElements.tsx
+++ components/QuoteGenerator/QuoteGeneratorElements.tsx
@@ -1,22 +1,52 @@
-import styled from 'styled-components';
-import Link from 'next/link';
-import Image from 'next/image';
+import styled from "styled-components";
+import Link from "next/link";
+import Image from "next/image";
 
 export const GradientBackgroundCon = styled.div`
-  background: linear-gradient(to right, #000046, #1cb5e0);
-  background-size: 400% 400%;
-  animation: gradient 6s ease infinite;
-  height: 100vh;
-  width: 100vw;
-  @keyframes gradient {
-    0% {
-      background-position: 0% 50%;
-    }
-    50% {
-      background-position: 100% 50%;
-    }
-    100% {
-      background-position: 0% 50%;
-    }
-  }
+	background: linear-gradient(to right, #000046, #1cb5e0);
+	background-size: 400% 400%;
+	animation: gradient 6s ease infinite;
+	height: 100vh;
+	width: 100vw;
+	@keyframes gradient {
+		0% {
+			background-position: 0% 50%;
+		}
+		50% {
+			background-position: 100% 50%;
+		}
+		100% {
+			background-position: 0% 50%;
+		}
+	}
+`;
+
+export const BackgroundImage1 = styled(Image)`
+	position: relative;
+	z-index: 1;
+	margin-left: 40px;
+	margin-top: 40px;
+`;
+
+export const BackgroundImage2 = styled(Image)`
+	position: fixed;
+	z-index: 1;
+	right: 40px;
+	bottom: 40px;
+`;
+
+export const FooterCon = styled.div`
+	width: 100vw;
+	height: 50px;
+	text-align: center;
+	font-family: "Source Code Pro", monospace;
+	font-size: 15px;
+	position: absolute;
+	bottom: 0;
+	color: #fff;
+	z-index: 999999;
+`;
+
+export const FooterLink = styled(Link)`
+	color: #fff;
 `;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,17 @@
+import { useState } from 'react'
+
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import { GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Components
+import { BackgroundImage1,  BackgroundImage2,FooterCon,FooterLink,GradientBackgroundCon } from '@/components/QuoteGenerator/QuoteGeneratorElements'
+
+// Assets
+import Clouds1 from '@/assets/cloud-and-tunder.png'
+import Clouds2 from '@/assets/cloudy-weather.png'
 
 export default function Home() {
+  const [numberOfQuotes, setNumberOfQuotes] = useState<Number | null>(0)
   return (
     <>
       <Head>
@@ -13,7 +21,45 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {/* Background */}
-      <GradientBackgroundCon></GradientBackgroundCon>
+      <GradientBackgroundCon>
+        <BackgroundImage1 src={Clouds1}
+          height={200}
+          alt='cloudy background1'
+        />
+
+        {/* Quote Generator Modal pop-up */}
+        {/* <QuoteGeneratorModal /> */}
+
+        {/* Quote Generator */}
+        <QuoteGeneratorCon>
+          <QuoteGeneratorInnerCon>
+            <QuoteGeneratorTitle>Daily Inspirational Generator</QuoteGeneratorTitle>
+            
+            <QuoteGeneratorSubtitile>
+              Looking for a splash of inspiration? Generator a quote card with a random inspirational quote provided by <FooterLink href="https://zenquotes.io/" target='_blank' rel='noopener noreferrer'>ZenQuotes API</FooterLink>
+            </QuoteGeneratorSubtitile>
+
+            <GenerateQuoteButton>
+              <GenerateQuoteButtonText onClick={null}>Make a quote</GenerateQuoteButtonText>
+            </GenerateQuoteButton>
+          </QuoteGeneratorInnerCon>
+        </QuoteGeneratorCon>
+
+        {/* Background Images*/}
+        <BackgroundImage2 src={Clouds2}
+          height={200}
+          alt='cloudy background2'
+        />
+
+        {/* Footer */}
+        <FooterCon>
+          <>
+          Quotes Generated: {numberOfQuotes}
+          <br/>
+          Developed with ❤️ by <FooterLink href="https://github.com/Mujeeb4582" target='_blank' rel='noopener noreferrer'>@Mujeeb-ur-Rahman</FooterLink>
+          </>
+        </FooterCon>
+      </GradientBackgroundCon>
     </>
   )
 }
